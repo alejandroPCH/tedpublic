39ae9240a8b1c53455ceb1e67f9bf4f106bd5327
alejandro <alejandropachas1@gmail.com>
2021-06-21 12:59:33 -0500

    i've started using states, also, some styles on the form were added

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,25 +1,79 @@
 import React from 'react'
+import '../styles/BadgeForm.css'
+
 
 class BadgeForm extends React.Component{
 
+    state={}
 
+    handleSummit=(e)=>{
+
+        console.log("El botón fue presionado")
+
+        console.log(this.state)
+        e.preventDefault()    
+
+    }
+
+
+    handleChange=(e)=>{
+        
+        
+        // console.log(e.target.value)
+
+        this.setState({ 
+
+            [e.target.name]:e.target.value,
+        })
+        console.log(this.state)
+
+    }
+
+   
+
+    
     render(){
 
-        return(<div>
+        return(<>
+
+            <form className="container__form" onSubmit={this.handleSummit} action="">
             <h1>New Attendant</h1>
 
-            <form action="">
-                <div className="form">
-                <label htmlFor="">
-                    First Name
-                </label>
-                <input type="text" className="form_control" name="first_name" />
+                <div className="form firstName">
+                
+                    <label htmlFor="">First Name</label>
+                    
+                    <input /*on react you have an "onChange" event on an input*/
+                         onChange={this.handleChange} type="text" className="form--input" name="firstName" value={this.state.FirstName} />
 
                 </div>
 
-                <button>Save</button>
+                <div className="form lastName">
+
+                    <label htmlFor="">Last Name</label>
+                    
+                        <input onChange={this.handleChange} type="text" className="form--input" name="lastName" value={this.state.LastName} /> 
+                        </div>
+
+                <div className="form jobTitle">
+                         
+
+                     <label htmlFor="">Job Title</label>
+                    
+                        <input onChange={this.handleChange} type="text" className="form--input" name="jobTitle" value={this.state.JobTitle} /> 
+                        </div>
+
+                <div className="form twitter">
+
+                    <label htmlFor="">Twitter</label>
+                    
+                    <input onChange={this.handleChange} type="text" className="form--input" name="twitter" value={this.state.Twitter} /> 
+                    
+                </div>
+
+                <button onClick={this.handleClick} className="container__button">Save</button>
             </form>
-        </div>
+        </>
         )
     }
 
