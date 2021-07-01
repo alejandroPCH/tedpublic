8ab91942c4c05411baa356e81ef126f4ea764d0a
alejandro <alejandropachas1@gmail.com>
2021-06-30 19:37:40 -0500

    does it work?x3

--- src/components/App.js
+++ src/components/App.js
@@ -16,13 +16,13 @@ import BadgeDetails from '../pages/BadgeDetails'
         <Layout>
 
             <Switch>
-                <Route exact path="tedpublic" component={MainPage} />
-                <Route exact path="tedpublic/:badgeId" component={BadgeDetails} />
+                <Route exact path="/tedpublic" component={MainPage} />
+                <Route exact path="/tedpublic/:badgeId" component={BadgeDetails} />
 
-                <Route exact path="tedpublic/new" component={NewAttendance} />
+                <Route exact path="/tedpublic/new" component={NewAttendance} />
 
 
-                <Route exact path="tedpublic/:badgeId/edit" component={BadgeEdit} />
+                <Route exact path="/tedpublic/:badgeId/edit" component={BadgeEdit} />
                 <Route component={NotFound}/>
 
             </Switch> 
